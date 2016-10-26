bc814aa042faa009a89eec2cfc3e7d3494deb175
limzykenneth <limzy.kenneth@gmail.com>
2016-10-26 20:03:19 +0800

    Clean up and prepare for new app

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -2,37 +2,7 @@ var $ = require('jquery');
 var _ = require('underscore');
 
 $(document).ready(function() {
-	var waitingGifs = [];
-
-	$.get('http://api.giphy.com/v1/gifs/search?q=waiting&api_key=dc6zaTOxFJmzC', function(data) {
-		_.each(data.data, function(el, i){
-			waitingGifs.push(el.images.downsized.url);
-		});
-	});
-
-	$("#waiting-gif").hover(function() {
-		gifDisplay(true, waitingGifs);
-	}, function() {
-		gifDisplay(false);
-	});
-
-	$("#mc-embedded-subscribe").click(function(e) {
-		if($("#mce-EMAIL").val() === "" || $("#mce-FNAME").val() === "" || $("mce-MMERGE9").val() === ""){
-			$(".response").css("display", "none");
-		}
-	});
-
-	$("#mce-responses .response").click(function(e){
-		$(this).css('display', 'none');
+	$("#page-content .grid").masonry({
+		itemSelector: ".grid-item"
 	});
 });
-
-function gifDisplay(dis, gifList){
-	if(dis){
-		var selected = gifList[_.random(0, gifList.length)];
-		$(".gif-popup img").attr("src", selected)
-			.parent(".gif-popup").css("display", "block");
-	}else{
-		$(".gif-popup").css("display", "none");
-	}
-}
