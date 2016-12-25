d6aec4063dfc857c6601e3d3536d7155727c6a92
limzykenneth <limzy.kenneth@gmail.com>
2016-12-25 17:01:16 +0800

    Let's say this work for now, yes it's not tested...

--- javascripts/genericCollection.js
+++ javascripts/genericCollection.js
@@ -9,46 +9,16 @@ var collection = Backbone.Collection.extend({
 	category: "Uncategorized",
 
 	initialize: function(models, app){
-
+		this.currentPage = 1;
 	},
 
-	getNextPage: function(page, callback){
-		// Came from nextPage() in view
-		var defer = [];
+	fetchNextPage: function(page, callback){
 		var types = ["features", "opportunities", "events", "directories"];
-		var original = {};
+		var defer = [];
 
 		_.each(types, function(el, i){
 			var col = window.app[el].collection;
-
-			original[el] = col.length;
-			if (col.currentPage < col.totalPages){
-				defer.push(col.fetchNextPage(col.currentPage + 1));
-			}else{
-				defer.push(el);
-			}
-		}, this);
-
-		var self = this;
-		$.when.apply($, defer).then(function(){
-			var nextPageCollection = [];
-			_.each(defer, function(el, i){
-				var type;
-				if(typeof el != "string" && el.responseJSON.length !== 0){
-					type = el.responseJSON[0].appData;
-				}else if(typeof el == "string"){
-					type = el;
-				}
-
-				if(typeof type != "undefined"){
-					var col = window.app[type].collection;
-					var offsetLength = col.length - original[type];
-					nextPageCollection = nextPageCollection.concat(col.slice(0, offsetLength));
-				}
-			});
-
-			self.add(nextPageCollection);
-			callback(nextPageCollection);
+			col.fetchNextPage();
 		});
 	}
 });
--- javascripts/genericCollectionView.js
+++ javascripts/genericCollectionView.js
@@ -16,6 +16,8 @@ var dView = new dirView();
 var view = Backbone.View.extend({
 	initialize: function(collection){
 		this.collection = collection;
+		this.listenTo(this.collection, "add", this.nextPage);
+		this.renderedItemsNumber = 0;
 	},
 
 	// Collection passed in should be in the right order
@@ -26,7 +28,10 @@ var view = Backbone.View.extend({
 		return this.$el.html();
 	},
 
+	// Will be overwritten by the children views
 	addModel: function(model){
+		this.renderedItemsNumber++;
+
 		var type = model.get("appData");
 		if(type == "features"){
 			this.$el.prepend(mView.render(model));
@@ -39,71 +44,15 @@ var view = Backbone.View.extend({
 		}
 	},
 
-	nextPage: function(callback){
-		// This is step 1
-		var app = window.app;
-		var col = new Backbone.Collection();
-		col.add(app.features.collection.toJSON());
-		col.add(app.opportunities.collection.toJSON());
-		col.add(app.events.collection.toJSON());
-		col.add(app.directories.collection.toJSON());
+	nextPage: function(){
+		var renderedItemsNumber = this.renderedItemsNumber;
+		var toRenderItemsNumber = this.collection.length - this.renderedItemsNumber;
 
-		var difference = function(array){
-			var rest = Array.prototype.concat.apply(Array.prototype, Array.prototype.slice.call(arguments, 1));
-			var containsEquals = function(obj, target) {
-				if (obj === null) return false;
-				return _.any(obj, function(value) {
-		  			return _.isEqual(value, target);
-				});
-			};
+		var toRenderItems = this.collection.slice(this.collection.length - toRenderItemsNumber, this.collection.length);
 
-			return _.filter(array, function(value){ return ! containsEquals(rest, value); });
-		};
+		_.each(toRenderitems, addModel, this);
 
-		var diff = difference(col.toJSON(), this.collection.toJSON());
-		if (diff.length > 10){
-			diff = diff.slice(0, 10);
-		}
-		this.collection.add(diff);
-
-		var diffCollection = new Backbone.Collection(diff);
-		var append = "";
-
-		diffCollection.each(function(model, i){
-			var type = model.get("appData");
-			if(type == "features"){
-				append += mView.render(model);
-			}else if(type == "events"){
-				append += eView.renderWithFullCategory(model);
-			}else if(type == "opportunities"){
-				append += oView.renderWithFullCategory(model);
-			}else if(type == "directories"){
-				append += dView.render(model);
-			}
-		});
-
-		callback(append);
-
-		if(diff.length === 0){
-			this.collection.getNextPage(this.collection.currentPage, function(nextPageCollection){
-				// Came back from collection
-				_.each(nextPageCollection, function(model, i){
-					var type = model.get("appData");
-					if(type == "features"){
-						append += mView.render(model);
-					}else if(type == "events"){
-						append += eView.renderWithFullCategory(model);
-					}else if(type == "opportunities"){
-						append += oView.renderWithFullCategory(model);
-					}else if(type == "directories"){
-						append += dView.render(model);
-					}
-				});
-
-				callback(append);
-				// Going back
-			});
-		}
+		return this.$el.html();
 	}
 });
 
